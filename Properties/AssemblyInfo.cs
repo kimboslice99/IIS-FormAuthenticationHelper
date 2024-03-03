25a756079da6165a10968afcb3d87a57f1b2ac5f
kimboslice99 <94807745+kimboslice99@users.noreply.github.com>
2024-03-03 14:13:22 -0500

    Update AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FormAuthenticationHelper")]
+[assembly: AssemblyTitle("FormsAuthenticationHelper")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FormAuthenticationHelper")]
+[assembly: AssemblyProduct("FormsAuthenticationHelper")]
 [assembly: AssemblyCopyright("Copyright © kimboslice99 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
